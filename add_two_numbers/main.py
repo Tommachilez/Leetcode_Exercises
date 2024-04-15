3d53affc9fcd5292c95f9e685ec606c03f89c830
Luan <ntluan2004@gmail.com>
2024-04-15 23:38:57 +0700

    add add two numbers exercise

--- add_two_numbers/main.py
+++ add_two_numbers/main.py
@@ -4,7 +4,6 @@ class ListNode:
         self.val = val
         self.next = nextnode
 
-
 class Solution:
     def addtwonumbers_approach_1(self, l1: ListNode, l2: ListNode) -> ListNode:
         num1 = 0
@@ -79,44 +78,5 @@ class Solution:
             current_node = current_node.next
             current_node.val = carry
             current_node.next = None
-
-        return result
-
-
-
-
-
-    def addtwonumbers_approach_1(self, l1: ListNode, l2: ListNode) -> ListNode:
-        num1 = 0
-        num2 = 0
-        i1 = 0
-        i2 = 0
-
-        while l1 is not None:
-            num1 += 10 ** i1 * l1.val
-            l1 = l1.next
-            i1 += 1
-
-        while l2 is not None:
-            num2 += 10 ** i2 * l2.val
-            l2 = l2.next
-            i2 += 1
-
-        total = num1 + num2
-        s = str(total)
-        n = len(s)
-        result = ListNode()
-        current_node = result
-
-        for i in range(n):
-            current_node.val = int(s[n - 1 - i])
-
-            if i == n - 1:
-                current_node.next = None
-                break
-
-            node = ListNode()
-            current_node.next = node
-            current_node = current_node.next
 
         return result
